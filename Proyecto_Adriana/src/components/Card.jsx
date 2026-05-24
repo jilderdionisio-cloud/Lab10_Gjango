edb35aeb27ca6a865835f4fad9fdf2cdfd717203
Adriana Chinchayhuara <adriana.chinchayhuar@tecsup.edu.pe>
2026-05-24 12:02:26 -0500

    feat: handle likes state with click event

--- Proyecto_Adriana/src/components/Card.jsx
+++ Proyecto_Adriana/src/components/Card.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react'
+
 function Card({ title, description }) {
+  const [likes, setLikes] = useState(0)
+
+  const handleLikes = () => {
+    setLikes(likes + 1)
+  }
+
   return (
     <article className="card">
       <h2>{title}</h2>
+
       <p>{description}</p>
+
+      <button
+        className="like-button"
+        onClick={handleLikes}
+      >
+        Likes {likes}
+      </button>
     </article>
   )
 }
