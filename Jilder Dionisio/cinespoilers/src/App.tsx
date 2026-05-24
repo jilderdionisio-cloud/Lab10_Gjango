62719e16861050851b8129b65327ac5cdc2636dd
Jilder Dionisio <jilder.dionisio@tecsup.edu.pe>
2026-05-24 12:22:58 -0500

    Fix cinespoilers folder visibility on GitHub by tracking its files directly

---
+++ Jilder Dionisio/cinespoilers/src/App.tsx
@@ -0,0 +1,14 @@
+import Card from './components/Card'
+
+function App() {
+  return (
+    <main>
+      <h1>CineSpoilerS 🎬</h1>
+      <Card title="Jildr Dionisio " description="Frontend Developer" />
+      <Card title="Fe" description="Musician" />
+      <Card title="Hola" description="jjja" />
+    </main>
+  )
+}
+
+export default App
